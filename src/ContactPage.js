3327e428281d0fe41008e8852092e35537941326
Stefan Ubbink <Stefan.Ubbink@sidn.nl>
2020-01-27 18:19:43 +0100

    First working time entry

--- src/ContactPage.js
+++ src/ContactPage.js
@@ -1,8 +1,9 @@
 //import React from 'react';
 //const ContactPage = () => <div>This is an About Page</div>
+// Maybe https://jaredpalmer.com/formik/
 // https://reactjs.org/docs/forms.html
 // https://reactjs.org/docs/hooks-state.html
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function ContactPage() {
   const [name, setName] = useState("");
@@ -13,22 +14,77 @@ function ContactPage() {
           0x4e, 0x6e // N,n
         ];
 
+  useEffect(
+    () => {
+        if (keys.includes('n') || keys.includes('N')) {
+            setTime("NT");
+        } else {
+          switch (keys.length) {
+            case 1:
+              setTime("HH:MM:SS.0" + keys);
+              break;
+            case 2:
+              setTime("HH:MM:SS." + keys);
+              //setTime("HH:MM:SS.{0}".(keys));
+              break;
+            case 3:
+              setTime("HH:MM:0" + keys[0] + "." + keys.substring(1,3));
+              break;
+            case 4:
+              setTime("HH:MM:" + keys.substring(0,2) +
+                "." + keys.substring(2));
+              break;
+            case 5:
+              setTime("HH:M" + keys.substring(0,1) + 
+                ":" + keys.substring(1,3) + 
+                "." + keys.substring(3));
+              break;
+            case 6:
+              setTime("HH:" + keys.substring(0,2) + 
+                ":" + keys.substring(2,4) + 
+                "." + keys.substring(4));
+              break;
+            case 7:
+              setTime("H" + keys.substring(0,1) + 
+                ":" + keys.substring(1,3) + 
+                ":" + keys.substring(3,5) + 
+                "." + keys.substring(5));
+              break;
+            case 8:
+              setTime("" + keys.substring(0,2) + 
+                ":" + keys.substring(2,4) + 
+                ":" + keys.substring(4,6) + 
+                "." + keys.substring(6));
+              //setTime("HH:MM:" + keys.substring(0,1) + "." + keys.substring(7));
+              break;
+            default:
+              setTime(keys);
+              break;
+          }
+        }
+    },
+    [keys],
+  );
   function handleChange(event) {
     setName(event.target.value);
   }
 
   function handleTimeChange(event) {
-    console.log(event.keyCode);
+    //console.log(event.keyCode);
     if (allowedKeys.includes(event.keyCode)) {
       if (event.keyCode === 8) {
       //if (event.target.value === 'n' || event.target.value === 'N') {
       //  setTime("NT");
         setKeys("");
-        setTime("");
+        //setTime("");
       } else {
-        // HH:MM:SS.ss
-        setKeys(keys + event.key);
-        setTime(keys + event.key);
+        //if (keys === undefined or keys === "") {
+        //} else {
+          // HH:MM:SS.ss
+          setKeys(keys + event.key);
+          //console.log(keys.length);
+          //setTime(keys + event.key);
+        //}
       }
     }
   }
